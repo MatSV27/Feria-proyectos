148e7d0bbad32be9adcd17451d487e34ac28205b
MatSV27 <103799936+MatSV27@users.noreply.github.com>
2023-11-27 15:18:11 -0500

    Update modelof.py
    Modificación al codigo original

--- modelof.py
+++ modelof.py
@@ -8,7 +8,7 @@ import numpy as np
 from datetime import datetime
 import time
 import mediapipe as mp
-
+import pickle
 # Rutas de las carpetas
 categories = {
     "Alumno Matriculado": "C:\\Users\\USER\\Desktop\\Modelo\\Images\\Alumno Matriculado",
@@ -16,30 +16,11 @@ categories = {
     "Profesor": "C:\\Users\\USER\\Desktop\\Modelo\\Images\\Profesor",
     "Trabajador": "C:\\Users\\USER\\Desktop\\Modelo\\Images\\Trabajador"
 }
-
-# Inicializar mediapipe
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.5, min_tracking_confidence=0.5)
-mp_drawing = mp.solutions.drawing_utils
-ruta_destino = r'C:\Users\USER\Desktop\Modelo\FOTOMANO'
-
-# Diccionario para almacenar las codificaciones faciales y los nombres de las imágenes de cada categoría
-category_encodings = {}
-category_names = {}
-
-# Cargar las codificaciones faciales y los nombres de las imágenes de cada categoría
-for category, folder_path in categories.items():
-    image_files = [f for f in os.listdir(folder_path) if f.lower().endswith(('.jpg', '.jpeg', '.png'))]
-    category_encodings[category] = []
-
-    for img in image_files:
-        face_encodings = face_recognition.face_encodings(face_recognition.load_image_file(os.path.join(folder_path, img)))
-        
-        # Verificar si hay al menos una codificación facial
-        if face_encodings:
-            category_encodings[category].append(face_encodings[0])
-
-    category_names[category] = [os.path.splitext(img)[0] for img in image_files]
+# Al inicio del programa
+num_frames_procesados = 0
+capturas_exitosas = 0
+total_capturas_externas = 0
+start_time = time.time()
 
 # Crear la ventana de Tkinter
 window = tk.Tk()
@@ -49,7 +30,7 @@ ancho_pantalla = window.winfo_screenwidth()
 alto_pantalla = window.winfo_screenheight()
 
 # Cargar la imagen de fondo y escalarla al tamaño de la pantalla
-ruta_imagen = r"C:\Users\USER\Desktop\Modelo\Interfaz\Interfaz.jpg"
+ruta_imagen = r"C:\Users\USER\Desktop\Modelo\Interfaz\Interfaz1.jpg"
 imagen_fondo = Image.open(ruta_imagen)
 imagen_fondo = imagen_fondo.resize((ancho_pantalla, alto_pantalla), Image.ANTIALIAS if hasattr(Image, 'ANTIALIAS') else Image.LANCZOS)
 imagen_fondo = ImageTk.PhotoImage(imagen_fondo)
@@ -81,6 +62,19 @@ dni_var = tk.StringVar()
 dni_entry = tk.Entry(window, textvariable=dni_var, font=("Arial", 14))
 dni_entry.place(relx=0.92, rely=0.6450, anchor=tk.E)
 
+# Cargar las codificaciones faciales y los nombres de las imágenes
+with open('category_encodings.pickle', 'rb') as f:
+    category_encodings = pickle.load(f)
+
+with open('category_names.pickle', 'rb') as f:
+    category_names = pickle.load(f)
+
+# Inicializar mediapipe
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+mp_drawing = mp.solutions.drawing_utils
+ruta_destino = r'C:\\Users\\USER\\Desktop\\Modelo\\FOTOMANO'
+
 def procesar_video():
     ret, frame = cap.read()
 
@@ -95,13 +89,19 @@ def procesar_video():
             # Codificar el rostro actual
             face_frame_encodings = face_recognition.face_encodings(frame, known_face_locations=[face_location], model="cnn")[0]  # Puedes ajustar el modelo aquí
 
-            text = "Externo"
+            text_top = ""
+            text_bottom = ""
             color = (0, 0, 255)  # Rojo
+            accuracy = 0.0
 
             for category, encodings in category_encodings.items():
                 results = face_recognition.compare_faces(encodings, face_frame_encodings, tolerance=0.5)
                 if True in results:
-                    text = category + " - " + category_names[category][results.index(True)]
+                    # Obtener el nombre del alumno de la carpeta correspondiente
+                    alumno_name = category_names[category][results.index(True)]
+                    accuracy = face_recognition.face_distance(encodings, face_frame_encodings)[results.index(True)]
+                    text_top = f"{category} - {alumno_name}"
+                    text_bottom = f"Precisión: {1 - accuracy:.2%}"
                     if category == "Alumno Matriculado":
                         color = (0, 255, 0)  # Verde 
                     elif category == "Alumno no Matriculado":
@@ -111,10 +111,16 @@ def procesar_video():
                     elif category == "Trabajador":
                         color = (255, 255, 0)  # Celeste
                     break 
+            else:
+                # Si no se encontró coincidencia en ninguna categoría, considerarlo externo con precisión del 100%
+                text_top = "Externo"
+                text_bottom = "Precisión: 100%"
+                color = (0, 0, 255)  # Rojo
 
             cv2.rectangle(frame, (left, top), (right, bottom), color, 2)
-            cv2.putText(frame, text, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1)
-
+            cv2.putText(frame, text_top, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1)
+            cv2.putText(frame, text_bottom, (left, bottom + 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1)
+            
     # Pasar el marco a RGB
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
@@ -135,17 +141,16 @@ def procesar_video():
 
         # Si el pulgar está a la izquierda del nudillo del dedo índice, la mano está cerrada
         if thumb_tip < index_finger_pip and len(dni_var.get()) == 8:
-            ruta_destino = r'C:\Users\USER\Desktop\Modelo\FOTOMANO'
+            total_capturas_externas += 1
+            ruta_destino = r'C:\\Users\\USER\\Desktop\\Modelo\\FOTOMANO'
             # Obtener la fecha y hora actual
             ahora = datetime.now()
             formato_fecha_hora = ahora.strftime("%y-%m-%d_%I-%M-%p")
             # Construir el nombre del archivo con la fecha y hora
             nombre_archivo = f"{dni_var.get()}_{formato_fecha_hora}.jpg"
+            capturas_exitosas += 1
 
-            # Guardar la imagen en la ruta de destino
             cv2.imwrite(os.path.join(ruta_destino, nombre_archivo), frame)
-  
-            # Mostrar un mensaje indicando que la foto ha sido tomada
             print(f"Foto tomada y guardada como {nombre_archivo}")
 
     # Convertir el cuadro a formato PIL y luego a formato ImageTk
@@ -157,10 +162,17 @@ def procesar_video():
     label.image = photo
 
     window.update_idletasks()
+    # Calcular el tiempo promedio por cuadro
+    end_time = time.time()
+    tiempo_procesamiento_promedio = (end_time - start_time) / num_frames_procesados if num_frames_procesados > 0 else 0
+    print(f"Tiempo de procesamiento promedio por cuadro: {tiempo_procesamiento_promedio} segundos")
 
+    # Actualizar el tiempo de inicio para el próximo cálculo
+    start_time = time.time()
     # Llamar a esta función nuevamente después de 15 milisegundos
-    window.after(15, procesar_video)
+    window.after(50, procesar_video)
 
 # Iniciar el bucle principal de Tkinter
-window.after(15, procesar_video)
+window.after(50, procesar_video)
+
 window.mainloop()
