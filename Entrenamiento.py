02fd7d8e35ad5bb5009364bdeb2decc6ab795ea8
EdwinSotto12311 <97325341+EdwinSotto12311@users.noreply.github.com>
2023-11-29 15:21:50 -0500

    Add files via upload

---
+++ Entrenamiento.py
@@ -0,0 +1,50 @@
+import face_recognition
+import os
+import pickle
+
+# Rutas de las carpetas
+categories = {
+    "Alumno Matriculado": "C:\\Users\\USER\\Desktop\\Modelo\\Images\\Alumno Matriculado",
+    "Alumno no Matriculado": "C:\\Users\\USER\\Desktop\\Modelo\\Images\\Alumno no Matriculado",
+    "Profesor": "C:\\Users\\USER\\Desktop\\Modelo\\Images\\Profesor",
+    "Trabajador": "C:\\Users\\USER\\Desktop\\Modelo\\Images\\Trabajador"
+}
+
+# Diccionario para almacenar las codificaciones faciales y los nombres de las imágenes de cada categoría
+category_encodings = {}
+category_names = {}
+
+# Cargar las codificaciones faciales y los nombres de las imágenes de cada categoría
+for category, folder_path in categories.items():
+    subfolders = [f.path for f in os.scandir(folder_path) if f.is_dir()]
+    total_subfolders = len(subfolders)
+    used_subfolders = 0
+    category_encodings[category] = []
+    category_names[category] = []
+
+    for subfolder in subfolders:
+        image_files = [f for f in os.listdir(subfolder) if f.lower().endswith(('.jpg', '.jpeg', '.png'))]
+
+        for img in image_files:
+            face_encodings = face_recognition.face_encodings(face_recognition.load_image_file(os.path.join(subfolder, img)))
+            
+            # Verificar si hay al menos una codificación facial
+            if face_encodings:
+                category_encodings[category].append(face_encodings[0])
+                category_names[category].append(os.path.basename(subfolder))  # Añadir el nombre de la subcarpeta al diccionario
+
+        used_subfolders += 1
+        # Calcular el indicador de avance
+        progress = used_subfolders / total_subfolders
+        print(f'Avance: {progress * 100}%')
+        print(f'Subcarpeta "{os.path.basename(subfolder)}" acabada')  # Mostrar mensaje cuando se ha terminado una subcarpeta
+
+    print(f'Carpeta {category} acabada')  # Mostrar mensaje cuando se ha terminado una categoría
+
+# Guardar las codificaciones faciales y los nombres de las imágenes
+with open('category_encodings.pickle', 'wb') as f:
+    pickle.dump(category_encodings, f)
+
+with open('category_names.pickle', 'wb') as f:
+    pickle.dump(category_names, f)
+
